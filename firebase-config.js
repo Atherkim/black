f45e61d046443d346ea84c47fd40644de0ae5b81
Atherkim <rlaqls321@gmail.com>
2026-07-28 14:16:43 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDM_uFj1KL_-VXE9h3QAUG7k9ggZ2_KgLE",
   authDomain: "ather-4879f.firebaseapp.com",
+  databaseURL: "https://ather-4879f-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "ather-4879f",
   storageBucket: "ather-4879f.firebasestorage.app",
   messagingSenderId: "673788727472",
   appId: "1:673788727472:web:5381718e9b82dd5e97eb75"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
